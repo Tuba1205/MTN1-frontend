febac67c3914731068f16d2da2ea6ca2aa215fa4
Tuba1205 <mubariktuba.1205@gmail.com>
2025-04-24 18:45:35 +0500

    changes made on CreateUser

--- src/pages/CreateUser.jsx
+++ src/pages/CreateUser.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import AdminSidebar from "../components/AdminSidebar";
-import "../styles/createUser.css";
+import "../styles/CreateUser.css";
 
 const CreateUser = () => {
   const navigate = useNavigate();
